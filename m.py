c32d54c6609fad299499eca2404ecf3b5c66b4a2
ollkostin <ollkostin@gmail.com>
2018-04-11 21:34:52 +0300

    add resizing

--- m.py
+++ m.py
@@ -29,12 +29,25 @@ def main(argv):
         if file_path is not None:
             if os.path.isfile(file_path):
                 img = cv2.imread(argv[1])
+                img = resize_if_necessary(img)
                 img_out = process_image(img)
                 cv2.imshow(window_title, img_out)
                 cv2.waitKey(0)
         else:
             print("no file specified")
     pass
+
+
+def resize_if_necessary(img):
+    height, width = img.shape[:2]
+    h_coef = float(height / height_max) if height > height_max else 1
+    w_coef = float(width / width_max) if width > width_max else 1
+    x = round(1 - float(1 / w_coef), 1)
+    y = round(1 - float(1 / h_coef), 1)
+    coef = max(x, y)
+    if coef != 0.0:
+        img = cv2.resize(img, (0, 0), fx=coef, fy=coef)
+    return img
 
 
 if __name__ == '__main__':
